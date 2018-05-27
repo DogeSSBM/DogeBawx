2ed4f061b6a2fa1e1ea62e4324c1ef30850b4fc4
Doge <dogessbm@gmail.com>
2018-05-27 08:56:15 -0400

    Full Rewrite
    Code now works as intended.

--- DogeBawx.ino
+++ DogeBawx.ino
@@ -1,351 +1,338 @@
-#define LINE 14 //gamecube controller data line pin
-#define led 15
-#define ledOn digitalWriteFast(led, HIGH)
-#define ledOff digitalWriteFast(led, LOW)
+//typedef shortcuts (ignore these)
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
 
-//set your button binds to their corrosponding pin here
-#define BTN_S  8
-#define BTN_Y  9
-#define BTN_X  10
-#define BTN_B  11
-#define BTN_A  12
-#define BTN_L  5
-#define BTN_R  6
-#define BTN_Z  7
-#define BTN_DU 0
-#define BTN_DD 0
-#define BTN_DL 0
-#define BTN_DR 0
-#define A_U 1
-#define A_D 2
-#define A_L 3
-#define A_R 4
-#define C_U 0
-#define C_D 0
-#define C_L 0
-#define C_R 0
-#define T_L 0
-#define T_R 0
+#define LINE 23						// gamecube controller data line pin
+const u8 id[3] = { 0x09,0x00,0x00 };		// ID to send in response to
+								// init CMD
+// buffer to be sent to console
+// last 2 bytes only sent in response to origin CMD (must be 0x02 for some reason)
+u8 buff[10] = { 0x00,0x80,0x80,0x80,0x80,0x80,0x01,0x01,0x02,0x02 };
 
-#define CYCLE ARM_DWT_CYCCNT //cpu cycle counter. increments every cpu cycle
+// one bit takes 4us to read/write
+// quarterBit = number of CPU cycles in 1us
+const u32 _quarterBit = F_CPU / 1000000;		// 1/4 bit takes this number of
+								// cycles to read/write
+const u32 _halfBit = _quarterBit * 2;		// 1/2 bit takes this number of
+								// cycles to read/write
+const u32 _threeqBit = _halfBit + _quarterBit;	// 3/4 bit takes this number of
+								// cycles to read/write
+const u32 _oneBit = _halfBit * 2;			// 1 bit takes this number of
+								// cycles to read/write
+const u32 _twoBit = _oneBit * 2;			// 2 bits take this number of
+								// cycles to read/write
+u32 tempCycle;						// temp var to store CPU cycle
+#define cycReg (ARM_DWT_CYCCNT)			// CPU Cycle count Register
+#define markCycle() (tempCycle = cycReg)		// record the current CPU cycle
+								// for use later
+#define elapsedCycles() (cycReg - tempCycle)	// number of CPU cycles elapsed
+								// since markCycle() was recorded
 
-uint32_t oldCYCLE;	//temp timing var 
-						//(global to reduce memory alocation overhead)
+struct {
+	// byte 1
+	u8 errStat : 1;				// send first
+	u8 errLatch : 1;
+	u8 origin : 1;
+	u8 S : 1;	// Start
+	u8 Y : 1;
+	u8 X : 1;
+	u8 B : 1;
+	u8 A : 1;					// send last
 
-//one bit takes 4us to send
-const uint32_t QUARTER = clockCyclesPerMicrosecond();
-const uint32_t HALF = QUARTER + QUARTER;
-const uint32_t THREEQ = HALF + QUARTER;
-const uint32_t ONE = THREEQ + QUARTER;
-const uint32_t TWO = ONE + ONE;
+	// byte 2
+	u8 high1 : 1;				// send first
+	u8 L : 1;	// Digital L Trigger
+	u8 R : 1;	// Digital R Trigger
+	u8 Z : 1;
+	u8 Du : 1;	// D-pad Up
+	u8 Dd : 1;	// D-pad Down
+	u8 Dr : 1;	// D-pad Right
+	u8 Dl : 1;	// D-pad Left		send last
 
-//cmd from console. cmd[0] is the actual cmd. (if applicable) cmd[1]
-//is the read mode. (if applicable) cmd[2] is rumble info
-uint8_t cmd[3] = { 0, 0, 0 };
+	// byte 3 - byte 8
+	u8 ax;	// Left analog stick x axis
+	u8 ay;	// Left analog stick Y axis
+	u8 cx;	// C analog stick x axis
+	u8 cy;	// C analog stick y axis
+	u8 l;		// Analog L Trigger
+	u8 r;		// Analog R Trigger
+}report;
 
-//id of the device to emulate
-const uint8_t id[3] = { 0b10010000u,0b00000000u,0b00000000u };
+// CMD read from console
+struct {
+	u8 cmd;					// CMD from console
+	u8 readMode;				// appears after read CMD (0x40)
+	u8 rumbleInfo;				// appears after readMode, after read CMD
+	bool unknownCmd;				// TRUE if CMD matches no known CMDs
+							// remains TRUE, until reset by user
+}cmd;
 
-//buffer of bytes to be sent to the gamecube
-//we should not ever need to send more than 10 bytes
-uint8_t buffer[10] = {
-	(0b00000100u),(0b00000001u),(0b10000000u),(0b10000000u),
-	(0b10000000u),(0b10000000u),(0b00000001u),(0b00000001u),
-	(0b00000000u),(0b00000000u)
-};
-
-//sets buttons pinMode() to INPUT_PULLUP, and LINE to INPUT
-static void initPins() {
-	//buttons
-	for (uint8_t i = 0; i < 13; i++) {
-		pinMode(i, INPUT_PULLUP);
+// reads and returns one bit from LINE
+// (assumes LINE is already INPUT)
+inline bool readBit()
+{
+	while (digitalReadFast(LINE)) {
+		// start of bit starts on falling edge
 	}
-	pinMode(led, OUTPUT);
-	/*
-	//make sure LINE does not end up in INPUT_PULLUP
-	digitalWriteFast(LINE, LOW);
-	*/
-	//set LINE to INPUT by default
-	pinMode(LINE, INPUT);
+	markCycle();			//record CPU cycle of falling edge
+	while (!digitalReadFast(LINE)) {
+		// bit value is determined by how long LINE is held LOW by the console
+	}
+	// 1 if LINE was LOW for < halfBit
+	// 0 if LINE was LOW for > halfBit
+	return elapsedCycles() < _halfBit;
 }
 
-//reads digital buttons (assumes pinMode is INPUT_PULLUP)
-//and packs them into buffer
-static inline void updateBuffer() {
-	buffer[0] =
-		(0 << 0) |	/*error status bit*/
-		(0 << 1) |	/*error latch bit*/
-		(0 << 2) |	/*origin bit*/
-		((!digitalReadFast(BTN_S)) << 3) |
-		((!digitalReadFast(BTN_Y)) << 4) |
-		((!digitalReadFast(BTN_X)) << 5) |
-		((!digitalReadFast(BTN_B)) << 6) |
-		((!digitalReadFast(BTN_A)) << 7);
-
-	buffer[1] =
-		(1 << 0) |	/*always high bit*/
-		((!digitalReadFast(BTN_L )) << 1) |
-		((!digitalReadFast(BTN_R )) << 2) |
-		((!digitalReadFast(BTN_Z )) << 3) |
-		((!digitalReadFast(BTN_DU)) << 4) |
-		((!digitalReadFast(BTN_DD)) << 5) |
-		((!digitalReadFast(BTN_DR)) << 6) |
-		((!digitalReadFast(BTN_DL)) << 7);
-
-	//analog stick x axis byte
-	buffer[2] = 128u;
-	if (!digitalReadFast(A_L)) {
-		buffer[2] = 0u;
-	}
-	else if(!digitalReadFast(A_R)) {
-		buffer[2] = 255u;
-	}
-
-	buffer[3] = 128u;
-	//analog stick y axis byte
-	if (!digitalReadFast(A_U)) {
-		buffer[3] = 0u;
-	}
-	if (!digitalReadFast(A_D)) {
-		buffer[3] = 255u;
-	}
-	
-	//c stick x axis byte
-	buffer[4] = 128u;
-	if (!digitalReadFast(C_L)) {
-		buffer[4] = 0u;
-	}
-	if (!digitalReadFast(C_R)) {
-		buffer[4] = 255u;
-	}
-
-	//c stick y axis byte
-	buffer[5] = 128u;
-	if (!digitalReadFast(C_L)) {
-		buffer[5] = 0u;
-	}
-	if (!digitalReadFast(C_R)) {
-		buffer[5] = 255u;
-	}
-
-	//left trigger analog press byte
-	buffer[6] = (!digitalReadFast(T_L)) ? 255 : 0;
-
-	//right trigger analog press byte
-	buffer[7] = (!digitalReadFast(T_R)) ? 255 : 0;
-
-	//required for rspOrigin() for some reason
-	//TODO: figure out the purpose of these bytes
-	buffer[8] = 0; buffer[9] = 0;
-}
-
-//response to send in case of read cmd
-static inline void rspRead() {
-	//console expects an 8 byte response
-	for (uint8_t i = 0; i < 8; i++) {
-		sendByte(buffer[i]);
-	}
-}
-
-//response to send in case of origin cmd
-static inline void rspOrigin() {
-	//console expects a 10 byte response
-	for (uint8_t i = 0; i < 10; i++) {
-		sendByte(buffer[i]);
-	}
-}
-
-//response to send in case of init cmd
-static inline void rspInit() {
-	//console expects a 3 byte response
-	for (uint8_t i = 0; i < 3; i++) {
-		sendByte(id[i]);
-	}
-}
-
-//reads cmd from console.
-//responds based on the cmd.
-static inline void readWrite() {
-	updateBuffer();
-	cmd[0] = getByte();
-	//most likely
-	if (cmd[0] == 0x40) {
-		//read cmd has two additional bytes
-		cmd[1] = getByte();
-		cmd[2] = getByte();
-		getStop();		//stop bit
-		ledOn;
-		//pinMode() needs to be OUTPUT mode to write data to LINE
-		pinMode(LINE, OUTPUT);
-		rspRead();
-		//TODO: figure out what to do in case of no stop bit
-	}
-
-	//origin is 1 one byte
-	else if (cmd[0] == 0x41) {
-		getStop();//stop bit
-		//pinMode() needs to be OUTPUT mode to write data to LINE
-		pinMode(LINE, OUTPUT);
-		rspOrigin();
-		//TODO: figure out what to do in case of no stop bit
-	}
-
-	//init cmd is 1 byte
-	else if (cmd[0] == 0x00 || cmd[0] == 0xff) {
-		getStop();//stop bit
-		//pinMode() needs to be OUTPUT mode to write data to LINE
-		pinMode(LINE, OUTPUT);
-		rspInit();
-		//TODO: figure out what to do in case of no stop bit
+// writes one bit to LINE
+// (assumes LINE is already OUTPUT)
+inline void writeBit(bool bt /* bit to be written */)
+{
+	markCycle();	// record CPU cycle of the start of the bit
+	if (bt) {
+		digitalWriteFast(LINE, LOW);
+		while (elapsedCycles() < _quarterBit) {
+			// hold LINE LOW for 1/4 (1us) of the bit
+		}
+		digitalWriteFast(LINE, HIGH);
+		while (elapsedCycles() < _oneBit) {
+			// hold LINE HIGH for the rest of the bit
+		}
+		return;
 	}
 	else {
-		//TODO: figure out what to do in case of unknown cmd
-		pinMode(LINE, OUTPUT);
-		rspRead();
+		digitalWriteFast(LINE, LOW);
+		while (elapsedCycles() < _threeqBit) {
+			// hold LINE LOW for 3/4 (3us) of the bit
+		}
+		digitalWriteFast(LINE, HIGH);
+		while (elapsedCycles() < _oneBit) {
+			// hold LINE HIGH for the rest of the bit
+		}
+		return;
 	}
-	//send controller stop bit
-	sendStop();
-	//pinMode() set to INPUT by default to lower cmd reading overhead
-	pinMode(LINE, INPUT);
 }
 
-//reads until 1 bit is read, then returns the bit
-//(assumes pin is already INPUT)
-static inline bool getBit() {
-	while (digitalReadFast(LINE)) {
-		//wait for line to drop, signaling the start of a new bit
-	}
-	//record the cpu cycle where the line goes low
-	oldCYCLE = CYCLE;
-	while (!digitalReadFast(LINE)) {
-		//the ammount of time LINE spends LOW determines the value of the current bit
-	}
-	//1 bit takes 4us to read / write. if LINE is low for < 2us the bit is a 1, otherwise it is a 0
-	return(CYCLE - oldCYCLE) < HALF;
-}
-
-//reads for console stop bit (3us or THREEQ)
-//(assumes pin is already INPUT)
-static inline bool getStop() {
-	while (digitalReadFast(LINE)) {
-		//wait for line to drop, signaling the start of a new bit
-	}
-	//record the cpu cycle where the line goes low
-	oldCYCLE = CYCLE;
-	while (!digitalReadFast(LINE)) {
-		//the ammount of time LINE spends LOW determines the value of the current bit
-	}
-	//similar to a 1, but total time is THREEQ instead of ONE
-	return(CYCLE - oldCYCLE) < HALF;
-}
-
-//reads LINE for 1 byte and returns it. 8 bits in 1 byte.
-//(assumes pin is already INPUT)
-static inline uint8_t getByte() {
-	uint8_t byt;
+// writes one byte to LINE (MSB order)
+// (assumes LINE is already OUTPUT)
+inline void writeByte(u8 byt /* byte to be written */)
+{
 	for (uint8_t i = 0; i < 8; i++) {
-		//shifts byt over by 1 bit to make room for the next then
-		//bitwise or with the bit read from LINE
-		byt = (byt << 1) | getBit();
+		writeBit(byt & 0x80);
+		byt <<= 1;
+	}
+}
+
+// reads and returns one byte from LINE
+// (assumes LINE is already INPUT)
+inline u8 readByte()
+{
+	u8 byt;		// byte being read
+				// Console sends MSB first
+	for (u8 i; i < 8; i++) {
+		byt = (byt << 1) | readBit();
 	}
 	return byt;
 }
 
-//writes a controller stop bit to LINE (assumes pin is already OUTPUT)
-static inline void sendStop() {
-	//write LOW for 2us
-	digitalWriteFast(LINE, LOW);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < HALF) {
-		/*wait for 1/2 bit*/
-	}
-
-	//write HIGH for 2us
-	digitalWriteFast(LINE, HIGH);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < HALF) {
-		/*wait for 1/2 bit*/
+// waits for almost 2 init probes
+inline void align()
+{
+	u16 buffer = 0xffff;				// temp buffer to store bits
+	delay(100);						// delay long enough for console to
+								// probe for attached controller
+	pinMode(LINE, INPUT);
+	// wait for buffer to match nearly 2 probes
+	while (buffer != 0b0000001000000001) {
+		buffer = (buffer << 1) | readBit();
 	}
 }
 
-//writes a 1 to LINE (assumes pin is already OUTPUT)
-static inline void send1() {
-	//write LOW for 1us
-	digitalWriteFast(LINE, LOW);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < QUARTER) {
-		/*wait for 1/4 bit*/
+// resds CMD and any extra info (if expected) from LINE
+// sets LINE to INPUT
+// sets cmd.unknownCmd to true in case of read error or unknown cmd
+void getCmd()
+{
+	cmd.cmd = readByte();
+
+	// read CMD has extra info
+	if (cmd.cmd == 0x40) {
+		cmd.readMode = readByte();
+		cmd.rumbleInfo = readByte();
+		readBit();		//stopbit
+		return;
 	}
 
-	//write HIGH for 3us
-	digitalWriteFast(LINE, HIGH);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < THREEQ) {
-		/*wait for 3/4 bit*/
-	}
-}
-
-//writes a 0 to LINE (assumes pin is already OUTPUT)
-static inline void send0() {
-	//write LOW for 3us
-	digitalWriteFast(LINE, LOW);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < THREEQ) {
-		/*wait for 3/4 bit*/
-	}
-
-	//write HIGH for 1us
-	digitalWriteFast(LINE, HIGH);
-	oldCYCLE = CYCLE;
-	while ((CYCLE - oldCYCLE) < QUARTER) {
-		/*wait for 1/4 bit*/
+	// origin CMD, init CMD, and reset CMD are all one byte
+	else if (cmd.cmd == 0x41 || cmd.cmd == 0x00 || cmd.cmd == 0xff) {
+		readBit();
+		return;
 	}
 }
 
-//writes byt (least significant bit first) to LINE
-//(assumes pin is already OUTPUT)
-static inline void sendByte(uint8_t byt) {
-	for (uint8_t i = 0; i < 8; i++) {
-		//sends the least significant bit
-		byt & 0b1u ? send1() : send0();
-		//shifts the significant bit out
-		byt = byt >> 1;
+// button pins
+/*
+S  Y  X  B
+0  1  2  3
+
+A  L  R  Z
+4  5  6  7
+
+Du Dd Dr Dl
+8  9  10 11
+
+Al Ar Au Ad
+12 13 14 15
+
+Cl Cr Cu Cd
+16 17 18 19
+
+La Ra
+20 21
+*/
+
+// reads physical buttons and stores states in buffer
+// (assumes button pin modes are already INPUT)
+inline void read()
+{
+	int32_t dir = 1;					// used to calculate direction of analog stick
+
+	// buttons first byte
+	buff[0] = 0 |
+		((!digitalReadFast( 0)) << 4) |	// Start
+		((!digitalReadFast( 1)) << 3) |	// Y
+		((!digitalReadFast( 2)) << 2) |	// X
+		((!digitalReadFast( 3)) << 1) |	// B
+		((!digitalReadFast( 4)) << 0) ;	// A
+	
+	// buttons second byte
+	buff[1] = 0 | 
+		((!digitalReadFast( 5)) << 6) |	// L
+		((!digitalReadFast( 6)) << 5) |	// R
+		((!digitalReadFast( 7)) << 4) |	// Z
+		((!digitalReadFast( 8)) << 3) |	// Dpad-Up
+		((!digitalReadFast( 9)) << 2) |	// Dpad-Down
+		((!digitalReadFast(10)) << 1) |	// Dpad-Right
+		((!digitalReadFast(11)) << 0) ;	// Dpad-Left
+
+	// analog stick X axis byte
+	if (!digitalReadFast(12)) {					// A-Left
+		buff[2] = !digitalReadFast(22) ? 128 : 0;		// A-Right
 	}
+	else {
+		buff[2] = !digitalReadFast(22) ? 255 : 128;	// A-Right
+	}
+	// analog sick Y axis byte
+	if (!digitalReadFast(14)) {					// A-Up
+		buff[3] = !digitalReadFast(15) ? 128 : 255;	// A-Down
+	}
+	else {
+		buff[3] = !digitalReadFast(15) ? 0 : 128;		// A-Down
+	}
+
+	// C stick X axis byte
+	if (!digitalReadFast(16)) {					// C-Left
+		buff[4] = !digitalReadFast(17) ? 128 : 0;		// C-Right
+	}
+	else {
+		buff[4] = !digitalReadFast(17) ? 255 : 128;	// C-Right
+	}
+	// C sick Y axis byte
+	if (!digitalReadFast(18)) {					// C-Up
+		buff[5] = !digitalReadFast(19) ? 128 : 255;	// C-Down
+	}
+	else {
+		buff[5] = !digitalReadFast(19) ? 0 : 128;		// C-Down
+	}
+
+	// Left trigger analog byte
+	buff[6] = !digitalReadFast(20) ? 170 : 1;
+
+	// Right trigger analog byte
+	buff[7] = !digitalReadFast(21) ? 85 : 1;
 }
 
-//waits until LINE is idle high for 8us after reading a bit
-//this ensures we dont start reading in the middle of a cmd
-static inline void wait() {
-	while (1) {
-		while (!getBit()) {
-			//we need to start waiting after the last bit in a
-			//cmd so that we dont start and finish our timing
-			//right before a new cmd starts. cmds always end
-			//with a 1. so we wait for that.
+// sends response based on CMD
+inline void respond()
+{
+	// read CMD
+	if (cmd.cmd == 0x40) {
+		for (uint8_t i = 0; i < 8; i++) {
+			writeByte(buff[i]);
 		}
-		oldCYCLE = CYCLE;
-		while (digitalReadFast(LINE)) {
-			//gamecube controller data line is idle high
-			//one bit takes ~4us to send, so if LINE is high
-			//for 8us, we know that it must be idle
-			if (CYCLE - oldCYCLE > TWO) { return; }
+	}
+
+	// origin CMD
+	else if (cmd.cmd == 0x41) {
+		buff[0] &= 0b11111011;				//set origin bit
+		for (uint8_t i = 0; i < 10; i++) {
+			writeByte(buff[i]);
 		}
 	}
+
+	// init / reset CMD
+	else if (cmd.cmd == 0x00 || cmd.cmd == 0xff) {
+		for (uint8_t i = 0; i < 3; i++) {
+			writeByte(id[i]);
+		}
+	}
+
+	writeBit(1);					// stop bit
 }
 
-void setup() {
-	//disable hardware interupts to prevent isr overhead
-	cli();
-	//set up cpu cycle counter register
+// reads CMD from console and responds
+inline void write()
+{
+	cli();						// disable interrupts
+								// (timing is critical for this function)
+	pinMode(LINE, INPUT);
+	getCmd();						// read CMD
+	pinMode(LINE, OUTPUT);
+	respond();						// respond to CMD
+	sei();						// enable interrupts
+								// (end of timeing critical code)
+}
+
+// sets up cpu cycle counter register
+inline void initCounter()
+{
 	ARM_DEMCR |= ARM_DEMCR_TRCENA;
 	ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA;
-	//sets pinMode() for all pins to the correct mode
-	initPins();
-	//ensures cmd read is aligned
-	wait();
 }
 
-uint8_t cnt;
-void loop() {/*avoid overhead*/while (1) {
-	readWrite();
-}}
+// sets all button pin modes to INPUT
+// assumes all buttons are on pins lower than LINE
+inline void initBtns()
+{
+	for (uint8_t i = 0; i < LINE; i++) {
+		pinMode(i, INPUT_PULLUP);
+	}
+	pinMode(LINE, INPUT);
+}
+
+// gets everything ready				// sets up cpu cycle counter register
+inline void init()
+{
+	initCounter();					// sets up cpu cycle counter register
+	initBtns();						// sets all button pin modes to INPUT
+								// assumes all buttons are on pins < LINE
+	align();						// waits for almost 2 init probes
+	write();						// reads CMD from console and responds
+}
+
+void setup()
+{
+	/*Serial.begin(9600);
+	Serial.println("Hi number 1");
+	delay(1000);
+	Serial.println("Hi number 2\n");*/
+	init();
+}
+
+void loop()
+{
+	read();						// read physical buttons and 
+								// stores states in buffer
+	write();						// writes buffer to console
+}
